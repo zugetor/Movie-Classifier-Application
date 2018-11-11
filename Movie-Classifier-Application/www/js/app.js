06b3b8d870da1156880afeee7f650ba8fa9ef5bd
Narawit <dekter123456@gmail.com>
2018-11-11 16:36:57 +0700

    add pages and controllers

--- Movie-Classifier-Application/www/js/app.js
+++ Movie-Classifier-Application/www/js/app.js
@@ -3,9 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform,$rootScope) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs).
@@ -24,3 +25,68 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home.html',
+		controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('app.favorite', {
+    url: '/favorite',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/favorite.html',
+		controller: 'FavoriteCtrl'
+      }
+    }
+  })
+
+  .state('app.profile', {
+    url: '/profile',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/profile.html',
+		controller: 'ProfileCtrl'
+      }
+    }
+  })
+
+  .state('app.boxoffice', {
+    url: '/boxoffice',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/boxoffice.html',
+		controller: 'BoxofficeCtrl'
+      }
+    }
+  })
+
+  .state('app.showmovie', {
+    url: '/showmovie',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/showmovie.html',
+		controller: 'ShowmovieCtrl'
+      }
+    }
+  })
+
+	$ionicConfigProvider.backButton.text("");
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
+});
