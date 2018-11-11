c83c64b7e9b6f1b012fd42854ac60512c8767347
Narawit <dekter123456@gmail.com>
2018-11-11 16:36:57 +0700

    add pages and controllers

---
+++ Movie-Classifier-Application/www/js/controllers.js
@@ -0,0 +1,19 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal) {
+})
+
+.controller('HomeCtrl', function($scope, $ionicModal) {
+})
+
+.controller('FavoriteCtrl', function($scope, $ionicModal) {
+})
+
+.controller('ProfileCtrl', function($scope, $ionicModal) {
+})
+
+.controller('ShowmivieCtrl', function($scope, $ionicModal) {
+})
+
+.controller('BoxofficeCtrl', function($scope, $ionicModal) {
+})
